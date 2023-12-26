e2cc4362d9097c6a79f3aa60a554849eab35713b
thessalenejean-louis <tjeanlouis@ippon.fr>
2023-12-26 14:21:11 +0100

    Initial commit

---
+++ app/src/main/java/com/jeanloth/mobile/android/budgetbuddy/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.jeanloth.mobile.android.budgetbuddy.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue1 = Color(0xFF6AC3FB)
+val Blue2 = Color(0xFF4B70F3)
+val Purple1 = Color(0xFF814BF3)
